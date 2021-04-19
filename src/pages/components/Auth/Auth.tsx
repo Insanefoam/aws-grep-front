1560e9284050f13489b273b3a608494cd75eadfe
Insanefoam <spartaq123456789@gmail.com>
2021-04-19 20:41:27 +0600

    feat: auth page

--- src/pages/components/Auth/Auth.tsx
+++ src/pages/components/Auth/Auth.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styles from './Auth.module.scss';
+import fonts from 'styles/fonts.module.scss';
+import { AuthForm } from './components';
 
 function Auth() {
-  return <div>Auth page</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={fonts.largeTitleText}>AWS Grep</h1>
+      <h2 className={fonts.title2Text}>
+        Here you can easily search your objects in the AWS S3 repository
+      </h2>
+      <AuthForm />
+    </div>
+  );
 }
 
 export default Auth;
